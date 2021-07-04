9abc3b3d2b58d0b77fe526b9c49e1a5e3e8eaf63
Matias <matiasarnoldo42@gmail.com>
2021-07-03 23:25:43 -0400

    AvanceP3

--- Prueba/apps/paginapricipal/urls.py
+++ Prueba/apps/paginapricipal/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.principal, name='principal'),
     path('galeria/', views.galeria, name='galeria'),
-    path('/productos', views.productos, name='productos')
+    ##path('/productos', views.productos, name='productos')
 ]
 
